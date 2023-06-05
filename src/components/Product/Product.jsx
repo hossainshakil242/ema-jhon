cb595bb97943b224bad869d2c2f6b25617b6b476
hossainshakil242 <realshakil8@gmail.com>
2023-06-05 22:09:28 +0600

    selected item select

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props);
-    const {img, name, ratings, seller, price}=props.product;
+    // console.log(props);
+    const {id, img, name, ratings, seller, price} = props.product;
+    const handleAddtoCart = props.handleAddtoCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +15,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings}start</p>
             </div>
-            <button className='btn-cart'>Add To Cart</button>
+            <button onClick={()=>handleAddtoCart(props.product)} className='btn-cart'>Add To Cart</button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,11 +4,18 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect(()=>{
         fetch ('products.json')
         .then(res=> res.json())
         .then(data=> setProducts(data))
     },[]);
+
+    const handleAddtoCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     // console.log(products);
 
     return (
@@ -18,11 +25,13 @@ const Shop = () => {
                     products.map(product=> <Product
                     key={product.id}
                     product={product}
+                    handleAddtoCart={handleAddtoCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h2>Order Summary</h2>
+                <p>Selected Items:{cart.length}</p>
             </div>
 
         </div>
